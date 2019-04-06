c62423905b2c5ae2438ccd14e301a2c2f1900253
Tatsuya Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-06 06:32:46 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,6 @@
 def levitation_quiz
 	loop do
-	  puts " What is the spell that enacts levitation?"
+	  puts "What is the spell that enacts levitation?"
 	  answer = gets.chomp
 	  break if answer == "Wingardium Leviosa"
 	end
